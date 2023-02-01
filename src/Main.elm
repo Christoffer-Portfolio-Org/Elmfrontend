a83228119dc180f0d707dce2a831ba25d2967708
ChristofferAAU <cn19@student.aau.dk>
2023-02-01 14:20:48 +0100

    Added textfield

--- src/Main.elm
+++ src/Main.elm
@@ -3,25 +3,30 @@ module Main exposing (..)
 import Browser
 import Html exposing (..)
 import Html.Events exposing (onClick)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
 
 main =
     Browser.sandbox { init = init, update = update, view = view}
 
-type alias Model = Int
+type alias Model = {counter : Int, textcont : String}
 
 init : Model
-init = 0
+init = {counter = 0, textcont = ""}
 
-type Msg = Increment | Decrement
+type Msg = Increment | Decrement | Change String
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
     Increment ->
-      model + 1
+        { model | counter = model.counter + 1 }
 
     Decrement ->
-      model - 1
+        { model | counter = model.counter - 1}
+    
+    Change newtext ->
+        { model | textcont = newtext}
 
 
 -- VIEW
@@ -30,7 +35,8 @@ view : Model -> Html Msg
 view model =
   div []
     [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
+    , div [] [ text (String.fromInt model.counter) ]
     , button [ onClick Increment ] [ text "+" ]
     , p [] [text "Hello i am a paragraph"]
+    , input [ placeholder "textfield", value model.textcont, onInput Change ] []
     ]
